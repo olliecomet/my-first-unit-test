af148329b73f453a5b72463a56798d70da3e1295
olliecomet <leyechi@gmail.com>
2020-01-04 20:39:27 -0800

    fixed tabbing

--- test/tests.js
+++ test/tests.js
@@ -3,17 +3,17 @@ import add from '../add.js';
 const test = QUnit.test;
 
 test('adds two numbers', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
-    const x = 3;
-    const y = 4;
-    const expected = 7;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const sum = add(x, y);
-
-    //Assert
-    // What should the result be?
-    assert.equal(sum, expected);
+  //Arrange
+  // Set up your parameters and expectations
+  const x = 3;
+  const y = 4;
+  const expected = 7;
+  
+  //Act 
+  // Call the function you're testing and set the result to a const
+  const sum = add(x, y);
+  
+  //Assert
+  // What should the result be?
+  assert.equal(sum, expected);
 });
